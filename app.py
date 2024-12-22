4d4da27d79f679bf3b996f60572d01b6cedbc73a
jonyprachine123 <jonyprachinebangla@gmail.com>
2024-12-22 16:36:45 +0600

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def main():
     st.title("🖼️ Prachin Bangla  Image Compressor")
     st.markdown("### V1")
     
-    watermark_url = "https://github.com/jonyprachine123/img/blob/main/logo.webp"
+    watermark_url = "https://github.com/jonyprachine123/img/blob/main/llm.png"
     
     mode = st.radio("Processing Mode", ["Single Image", "Multiple Images"], horizontal=True)
     
